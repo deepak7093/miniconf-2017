0162ee5c87c56ed729bcb520ac59dbca08ebb753
Aman Bisht <amanbisht@Amans-MacBook-Air.local>
2017-12-08 07:21:16 +0530

    Added lb configs as well

--- terraform/ec2/igw.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_internet_gateway" "igw" {
-    vpc_id = "${aws_vpc.rootconf-vpc.id}"
-
-    tags {
-        "Name" = "rootconf-igw"
-    }
-}
--- terraform/ec2/rta.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_route_table_association" "rootconf-public-rt-subnet" {
-    route_table_id = "${aws_route_table.public-route-table.id}"
-    subnet_id = "${aws_subnet.public-subnet.id}"
-}
